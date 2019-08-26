9e6dad035e307011b48c5b39fad5b92747cd8bb1
AndriusK <akrisiun@gmail.com>
2019-08-26 20:24:30 +0300

    TODO htmlgen

---
+++ _htmlgen/src/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using SourceBrowser;
+
+namespace htmlgen
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ProgramLoad.isDebug = true;
+
+            //var corlib = @"D:\Beta\dotnet\Manifest\referencesource\mscorlib";
+            //var csproj = "mscorlib.csproj";
+            // Directory.SetCurrentDirectory(corlib);
+            // var exe = @"C:\bin\tools\htmlgen\HtmlGenerator.exe /out:srcweb $csproj";
+
+            var sln = @"D:\Beta\dotnet\Manifest";
+            var slnFile = "RuntimePortable.Source.sln";
+            var exe = $@"C:\bin\tools\htmlgen\HtmlGenerator.exe /out:srcweb2 {slnFile}";
+            Console.WriteLine(exe);
+            Directory.SetCurrentDirectory(sln);
+
+            ProgramLoad.Main(new string[] { "/out:srcweb2", slnFile });
+            
+            Console.WriteLine("END!");
+            Console.ReadKey();
+        }
+    }
+}
